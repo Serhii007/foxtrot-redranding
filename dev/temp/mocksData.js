45ac425fe979052540ecad6a023d391f5c9983fe
Serhii Kyslytsia <kyslytsia007@gmail.com>
2018-05-30 10:26:21 +0300

    PLP, PDP, HP with mobile header and footer)

---
+++ dev/temp/mocksData.js
@@ -0,0 +1,35 @@
+/* Module data structure */
+
+// moduleName: {
+//     dataType: {
+//         property: value
+//     }
+// }
+
+/* Module data example */
+
+_template: {
+    big: {
+        title: 'Hello world',
+        age: 10,
+        button: false
+    }
+},
+
+'head': {
+    defaults: {
+        title: 'default title',
+        useSocialMetaTags: true
+    }
+},
+
+__pages: [{
+                name: 'card',
+                href: 'card.html'
+             },{
+                name: 'index',
+                href: 'index.html'
+             },{
+                name: 'listing',
+                href: 'listing.html'
+             }]
